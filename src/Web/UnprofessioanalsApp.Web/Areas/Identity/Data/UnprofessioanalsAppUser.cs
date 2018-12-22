41f4cbbceae665489dbee1da83d38df36fd2a4fc
sapphire119 <sap.phire@abv.bg>
2018-12-22 12:22:36 +0200

    Added Identity

---
+++ src/Web/UnprofessioanalsApp.Web/Areas/Identity/Data/UnprofessioanalsAppUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace UnprofessioanalsApp.Web.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the UnprofessioanalsAppUser class
+    public class UnprofessioanalsAppUser : IdentityUser
+    {
+    }
+}
