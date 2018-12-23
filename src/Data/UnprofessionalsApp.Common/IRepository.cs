57576d0d473cc88f15beafe62edba92cfacb6802
sapphire119 <sap.phire@abv.bg>
2018-12-23 21:03:13 +0200

    Created IRepository.cs used for Repository Pattern

---
+++ src/Data/UnprofessionalsApp.Common/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnprofessionalsApp.Common
+{
+	public interface IRepository<TEntity>
+		where TEntity : class
+	{
+		//Select заявка
+		IQueryable<TEntity> All();
+
+		Task AddAsync(TEntity entity);
+
+		void Delete(TEntity entity);
+
+		Task<int> SaveChangesAync();
+	}
+}
