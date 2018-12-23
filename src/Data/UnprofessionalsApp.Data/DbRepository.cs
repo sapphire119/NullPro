010625b9761bc3d2186b6d1e1a9ecd3f55a499a4
sapphire119 <sap.phire@abv.bg>
2018-12-23 21:03:57 +0200

    Implemented IRpoestory.cs to DbRepository.cs

---
+++ src/Data/UnprofessionalsApp.Data/DbRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UnprofessionalsApp.Common;
+
+namespace UnprofessionalsApp.Data
+{
+	public class DbRepository<TEntity> : IRepository<TEntity>, IDisposable
+		where TEntity : class
+	{
+		private readonly UnprofessionalsDbContext context;
+		private DbSet<TEntity> dbset;
+
+		public DbRepository(UnprofessionalsDbContext context)
+		{
+			this.context = context;
+			this.dbset = this.context.Set<TEntity>();
+		}
+
+		public Task AddAsync(TEntity entity)
+		{
+			return this.dbset.AddAsync(entity);
+		}
+
+		public IQueryable<TEntity> All()
+		{
+			return this.dbset;
+		}
+
+		public void Delete(TEntity entity)
+		{
+			this.dbset.Remove(entity);
+		}
+
+		public Task<int> SaveChangesAync()
+		{
+			return this.context.SaveChangesAsync();
+		}
+
+		public void Dispose()
+		{
+			this.context.Dispose();
+		}
+	}
+}
