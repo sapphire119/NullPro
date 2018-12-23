08b23a4293a62e338c189c635fab2708b4246201
sapphire119 <sap.phire@abv.bg>
2018-12-23 17:29:51 +0200

    Successfully created Database.

--- src/Data/UnprofessionalsData.Data/UnprofessioanalsDbContext.cs
+++ src/Data/UnprofessionalsData.Data/UnprofessioanalsDbContext.cs
@@ -12,7 +12,8 @@
         {
         }
 
-        protected override void OnModelCreating(ModelBuilder builder)
+
+		protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed.
---
+++ src/Data/UnprofessionalsData.Data/UnprofessioanalsDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿namespace UnprofessionalsData.Data
+{
+	using System.IO;
+
+	using Microsoft.EntityFrameworkCore;
+	using Microsoft.EntityFrameworkCore.Design;
+	using Microsoft.EntityFrameworkCore.Diagnostics;
+	using Microsoft.Extensions.Configuration;
+
+	public class UnprofessioanalsDbContextFactory : IDesignTimeDbContextFactory<UnprofessioanalsDbContext>
+	{
+		public UnprofessioanalsDbContext CreateDbContext(string[] args)
+		{
+			var configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				.Build();
+
+			var builder = new DbContextOptionsBuilder<UnprofessioanalsDbContext>();
+
+			var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+			builder.UseSqlServer(connectionString)
+				.UseLazyLoadingProxies();
+
+			//Stop Client Querry evaluation
+			builder.ConfigureWarnings(w => w.Throw(RelationalEventId.QueryClientEvaluationWarning));
+
+			return new UnprofessioanalsDbContext(builder.Options);
+		}
+	}
+}
