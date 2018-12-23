c5d13538456ea134023c5965b65a9e3146cb8334
sapphire119 <sap.phire@abv.bg>
2018-12-23 19:39:13 +0200

    Successfully renamed UnprofessionalsApp.Data & UnprofessionalsApp.Models

---
+++ src/Data/UnprofessionalsApp.Models/UnprofessioanalsAppUser.cs
@@ -0,0 +1,12 @@
+﻿namespace UnprofessionalsApp.Models
+{
+	using Microsoft.AspNetCore.Identity;
+
+	// Add profile data for application users by adding properties to the UnprofessioanalsAppUser class
+	//install-package Microsoft.AspNetCore.Identity.EntityFrameworkCore
+	//install-package Microsoft.AspNetCore.Identity
+
+	public class UnprofessioanalsAppUser : IdentityUser
+    {
+    }
+}
