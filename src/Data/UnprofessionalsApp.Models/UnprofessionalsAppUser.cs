e21d52dd995c03d7d745b41c766562233e972b97
sapphire119 <sap.phire@abv.bg>
2018-12-24 16:01:23 +0200

    Added _Layout.cshtml to Identity area. Added logo png (not final)

--- src/Data/UnprofessionalsApp.Models/UnprofessionalsAppUser.cs
+++ src/Data/UnprofessionalsApp.Models/UnprofessionalsAppUser.cs
@@ -5,5 +5,6 @@ namespace UnprofessionalsApp.Models
 	// Add profile data for application users by adding properties to the UnprofessionalsAppUser class
 	public class UnprofessionalsAppUser : IdentityUser<int>
 	{
+
 	}
 }
