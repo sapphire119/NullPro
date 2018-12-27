dd237cf44eb60ed290a1854ddffde6e1c36261d9
sapphire119 <sap.phire@abv.bg>
2018-12-27 21:37:49 +0200

    Fixed small bug. Added Areas for Administrator and Manager

--- src/Web/UnprofessionalsApp.Web/Startup.cs
+++ src/Web/UnprofessionalsApp.Web/Startup.cs
@@ -95,6 +95,9 @@ namespace UnprofessionalsApp.Web
 				routes.MapRoute(
 					name: "default",
 					template: "{controller=Home}/{action=Index}/{id?}");
+				routes.MapRoute(
+					name: "areas",
+					template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 			});
 
 		}
