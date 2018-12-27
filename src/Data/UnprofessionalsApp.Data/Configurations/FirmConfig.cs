15440f99db88fff4235be2b02eb3cef830183dec
sapphire119 <sap.phire@abv.bg>
2018-12-27 19:44:59 +0200

    Created FirmConfig.cs for OnModelCreationMethod

---
+++ src/Data/UnprofessionalsApp.Data/Configurations/FirmConfig.cs
@@ -0,0 +1,32 @@
+﻿namespace UnprofessionalsApp.Data.Configurations
+{
+	using Microsoft.EntityFrameworkCore;
+	using Microsoft.EntityFrameworkCore.Metadata.Builders;
+	using UnprofessionalsApp.Models;
+
+	public class FirmConfig : IEntityTypeConfiguration<Firm>
+	{
+		public void Configure(EntityTypeBuilder<Firm> builder)
+		{
+			builder.HasKey(e => e.Id);
+
+			builder.Property(e => e.UniqueFirmId);
+
+			builder.Property(e => e.Name);
+
+			builder.Property(e => e.Popularity);
+
+			builder.Property(e => e.Rating);
+
+			builder.Property(e => e.UrlToTradersRegistry);
+
+			builder.Property(e => e.IsBlackListed);
+
+			builder.HasOne(e => e.User)
+				.WithMany(u => u.Firms);
+
+			builder.HasOne(e => e.Category)
+				.WithMany(c => c.Firms);
+		}
+	}
+}
