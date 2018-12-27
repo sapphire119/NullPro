587b79a6ffb3f75849514d83c3671468286f21f9
sapphire119 <sap.phire@abv.bg>
2018-12-27 19:44:29 +0200

    Moved ErrorViewModel to InputViewModel.csproj Fixed references, Started Applying configuration classes for Fluent Api

--- src/Data/UnprofessionalsApp.Models/UnprofessionalsAppUser.cs
+++ src/Data/UnprofessionalsApp.Models/UnprofessionalsAppUser.cs
@@ -1,26 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
 
 namespace UnprofessionalsApp.Models
 {
 	// Add profile data for application users by adding properties to the UnprofessionalsAppUser class
 	public class UnprofessionalsAppUser : IdentityUser<int>
 	{
-		public bool IsBlackListed { get; set; } = false;
 
-		public DateTime DateOfRegistration { get; set; } = DateTime.UtcNow;
-
-		public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
-
-		public virtual ICollection<Comment> Comments { get; set; } = new HashSet<Comment>();
-
-		public virtual ICollection<Reply> Replies { get; set; } = new HashSet<Reply>();
-
-		public virtual ICollection<Message> SentMessages { get; set; } = new HashSet<Message>();
-
-		public virtual ICollection<Message> RecievedMessages { get; set; } = new HashSet<Message>();
-
-		public virtual ICollection<Firm> Firms { get; set; } = new HashSet<Firm>();
 	}
 }
