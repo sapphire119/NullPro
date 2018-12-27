45dc1670b3aae17152485c32a93613bb60a3e250
sapphire119 <sap.phire@abv.bg>
2018-12-27 19:44:47 +0200

    Created CommentsConfig.cs for OnModelCreationMethod

---
+++ src/Data/UnprofessionalsApp.Data/Configurations/CommentsConfig.cs
@@ -0,0 +1,28 @@
+﻿namespace UnprofessionalsApp.Data.Configurations
+{
+	using Microsoft.EntityFrameworkCore;
+	using Microsoft.EntityFrameworkCore.Metadata.Builders;
+	using UnprofessionalsApp.Models;
+
+	public class CommentsConfig : IEntityTypeConfiguration<Comment>
+	{
+		public void Configure(EntityTypeBuilder<Comment> builder)
+		{
+			builder.HasKey(e => e.Id);
+
+			builder.Property(e => e.Description);
+
+			builder.Property(e => e.Rating);
+
+			builder.Property(e => e.DateOfCreation);
+
+			builder.HasOne(e => e.User)
+				.WithMany(u => u.Comments)
+				.OnDelete(DeleteBehavior.Restrict);
+
+			builder.HasOne(e => e.Post)
+				.WithMany(p => p.Comments)
+				.OnDelete(DeleteBehavior.Restrict);
+		}
+	}
+}
