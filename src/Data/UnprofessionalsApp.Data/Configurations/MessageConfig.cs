7772c260b4cf857bd20819fceaed5a8ee1b609d8
sapphire119 <sap.phire@abv.bg>
2018-12-27 19:45:13 +0200

    Created MessageConfig.cs for OnModelCreationMethod

---
+++ src/Data/UnprofessionalsApp.Data/Configurations/MessageConfig.cs
@@ -0,0 +1,28 @@
+﻿namespace UnprofessionalsApp.Data.Configurations
+{
+	using Microsoft.EntityFrameworkCore;
+	using Microsoft.EntityFrameworkCore.Metadata.Builders;
+	using UnprofessionalsApp.Models;
+
+	public class MessageConfig : IEntityTypeConfiguration<Message>
+	{
+		public void Configure(EntityTypeBuilder<Message> builder)
+		{
+			builder.HasKey(e => e.Id);
+
+			builder.Property(e => e.Description);
+
+			builder.Property(e => e.DateOfCreation);
+
+			builder.HasOne(e => e.Sender)
+				.WithMany(u => u.SentMessages)
+				.HasForeignKey(e => e.SenderId)
+				.OnDelete(DeleteBehavior.Restrict);
+
+			builder.HasOne(e => e.Reciever)
+				.WithMany(u => u.RecievedMessages)
+				.HasForeignKey(e => e.ReceiverId)
+				.OnDelete(DeleteBehavior.Restrict);
+		}
+	}
+}
