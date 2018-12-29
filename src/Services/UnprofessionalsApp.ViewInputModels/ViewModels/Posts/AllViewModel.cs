9733b682c607aab635a6f333d106c774bffed684
sapphire119 <sap.phire@abv.bg>
2018-12-30 01:54:37 +0200

    Created AllViewModel.cs to render all posts

---
+++ src/Services/UnprofessionalsApp.ViewInputModels/ViewModels/Posts/AllViewModel.cs
@@ -0,0 +1,53 @@
+﻿namespace UnprofessionalsApp.ViewInputModels.ViewModels.Posts
+{
+	using System;
+	using System.Globalization;
+	using System.Net;
+	using AutoMapper;
+	using UnprofessionalsApp.Mapping.Contracts;
+	using UnprofessionalsApp.Models;
+	using UnprofessionalsApp.ViewInputModels.Extension;
+
+	public class AllViewModel : IMapFrom<Post>, IHaveCustomMappings
+	{
+		private string imageUrl;
+		private string dateOfCreation;
+
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public string DateOfCreation { get; set; }
+
+		public int UserId { get; set; }
+
+		public string ImageUrl
+		{
+			get
+			{
+				return this.imageUrl;
+			}
+			set
+			{
+				this.imageUrl = WebUtility.UrlDecode(value);
+			}
+		}
+
+		public void CreateMappings(IMapperConfigurationExpression configuration)
+		{
+			configuration.CreateMap<Post, AllViewModel>()
+				.ForMember(x => x.DateOfCreation,
+					x => x.MapFrom(
+							p => p.DateOfCreation.ToString(@"d MMMM, yyyy", CultureInfo.InvariantCulture)))
+				.ForMember(x => x.Description, 
+					x => x.MapFrom(
+							p => p.Description.Length > ProjectConsants.AllowedCharactersToRender ?
+								string.Concat(
+									p.Description.Substring(0, ProjectConsants.AllowedCharactersToRender),
+									ProjectConsants.DescriptionExtensionStrings) 
+										: p.Description));
+		}
+	}
+}
