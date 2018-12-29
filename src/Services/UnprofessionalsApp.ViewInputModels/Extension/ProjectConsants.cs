69ded2a1e98a0070ce87aa6687a316ebc97efde3
sapphire119 <sap.phire@abv.bg>
2018-12-30 01:54:15 +0200

    Introduced Project Assembly Constants for cleaner usage in current project

---
+++ src/Services/UnprofessionalsApp.ViewInputModels/Extension/ProjectConsants.cs
@@ -0,0 +1,9 @@
+﻿namespace UnprofessionalsApp.ViewInputModels.Extension
+{
+	public static class ProjectConsants
+	{
+		public const int AllowedCharactersToRender = 97;
+
+		public const string DescriptionExtensionStrings = "...";
+	}
+}
