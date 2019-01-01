0ee47ff3f510958ad82111145b4197ceed9d126c
sapphire119 <sap.phire@abv.bg>
2019-01-02 00:09:11 +0200

    Started Implemented Razor Pages. First web page is Firms/Index.cshtml

---
+++ src/Web/UnprofessionalsApp.Web/Pages/Firms/Index.cshtml.cs
@@ -0,0 +1,39 @@
+namespace UnprofessionalsApp.Web.Pages.Firms
+{
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.AspNetCore.Mvc.RazorPages;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Threading.Tasks;
+	using UnprofessionalsApp.DataServices.Contracts;
+	using UnprofessionalsApp.ViewInputModels.ViewModels.Firms;
+
+	public class IndexModel : PageModel
+    {
+		private readonly IFirmsService firmsService;
+
+		public IndexModel(IFirmsService firmsService)
+		{
+			this.firmsService = firmsService;
+		}
+
+		[BindProperty(SupportsGet = true)]
+		public int CurrentPage { get; set; } = 1;
+
+		public int Count { get; set; }
+
+		[BindProperty(SupportsGet = true)]
+		public int PageSize { get; set; } = 10;
+
+		public int TotalPages => (int)Math.Ceiling(decimal.Divide(Count, PageSize));
+
+		public List<FirmViewModel> Data { get; set; }
+
+		public void OnGetAsync()
+        {
+			this.Data = this.firmsService.GetAllFirmsForCurrentPage(CurrentPage, PageSize).ToList();
+			this.Count = this.firmsService.GetAllFirmsCount();
+        }
+    }
+}
