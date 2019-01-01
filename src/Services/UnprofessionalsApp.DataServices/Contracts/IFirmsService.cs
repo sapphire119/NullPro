7d83760824563bdf199f4f81314fb4a88af8c2b3
sapphire119 <sap.phire@abv.bg>
2019-01-02 00:07:50 +0200

    Started implementing FirmsService

---
+++ src/Services/UnprofessionalsApp.DataServices/Contracts/IFirmsService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnprofessionalsApp.ViewInputModels.ViewModels;
+using UnprofessionalsApp.ViewInputModels.ViewModels.Firms;
+
+namespace UnprofessionalsApp.DataServices.Contracts
+{
+	public interface IFirmsService
+	{
+		IEnumerable<FirmViewModel> GetAllFirmsForCurrentPage(int pageId, int pageSize);
+		
+		TViewModel GetFirmById<TViewModel>(int id);
+
+		int GetAllFirmsCount();
+	}
+}
---
+++ src/Services/UnprofessionalsApp.DataServices/FirmsService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnprofessionalsApp.Common;
+using UnprofessionalsApp.DataServices.Contracts;
+using UnprofessionalsApp.Mapping;
+using UnprofessionalsApp.Models;
+using UnprofessionalsApp.ViewInputModels.ViewModels;
+using UnprofessionalsApp.ViewInputModels.ViewModels.Firms;
+
+namespace UnprofessionalsApp.DataServices
+{
+	public class FirmsService : IFirmsService
+	{
+		private readonly IRepository<Firm> firmsRepository;
+
+		public FirmsService(IRepository<Firm> firmsRepository)
+		{
+			this.firmsRepository = firmsRepository;
+		}
+
+		public int GetAllFirmsCount()
+		{
+			return this.firmsRepository.All().Count();
+		}
+
+		public IEnumerable<FirmViewModel> GetAllFirmsForCurrentPage(int currentPage, int pageSize)
+		{
+			//TODO: Test Me
+
+			var firmsForCurrentPage = this.firmsRepository.All()
+				.OrderBy(d => d.Id)
+				.Skip((currentPage - 1) * pageSize)
+				.Take(pageSize)
+				.To<FirmViewModel>()
+				.ToList();
+
+			return firmsForCurrentPage;
+		}
+
+		public TViewModel GetFirmById<TViewModel>(int id)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
