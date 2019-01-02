a350fadc5063132e9745a1d47c8a612b618675a0
sapphire119 <sap.phire@abv.bg>
2019-01-02 21:12:45 +0200

    Changed DefaultImageUrl in ProjectConsants.cs Changed DateOfCreation entity Type to DateTime to better fit in view model

--- src/Services/UnprofessionalsApp.ViewInputModels/Extension/ProjectConsants.cs
+++ src/Services/UnprofessionalsApp.ViewInputModels/Extension/ProjectConsants.cs
@@ -5,5 +5,7 @@
 		public const int AllowedCharactersToRender = 97;
 
 		public const string DescriptionExtensionStrings = "...";
+
+		public const string DefaultImageUrl = @"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/No_image_3x4.svg/1024px-No_image_3x4.svg.png";
 	}
 }
--- src/Services/UnprofessionalsApp.ViewInputModels/ViewModels/Posts/PostViewModel.cs
+++ src/Services/UnprofessionalsApp.ViewInputModels/ViewModels/Posts/PostViewModel.cs
@@ -11,7 +11,6 @@
 	public class PostViewModel : IMapFrom<Post>, IHaveCustomMappings
 	{
 		private string imageUrl;
-		private string dateOfCreation;
 
 		public int Id { get; set; }
 
@@ -19,7 +18,7 @@
 
 		public string Description { get; set; }
 
-		public string DateOfCreation { get; set; }
+		public DateTime DateOfCreation { get; set; }
 
 		public int UserId { get; set; }
 
@@ -39,15 +38,18 @@
 		{
 			//TODO: Test me somehow
 			configuration.CreateMap<Post, PostViewModel>()
-				.ForMember(x => x.DateOfCreation,
+				.ForMember(x => x.ImageUrl,
 					x => x.MapFrom(
-							p => p.DateOfCreation.ToString(@"d MMMM, yyyy", CultureInfo.InvariantCulture)))
-				.ForMember(x => x.Description, 
+						p => string.IsNullOrWhiteSpace(p.ImageUrl) ? ProjectConsants.DefaultImageUrl : p.ImageUrl))
+				//.ForMember(x => x.DateOfCreation,
+				//	x => x.MapFrom(
+				//			p => p.DateOfCreation.ToString(@"d MMMM, yyyy", CultureInfo.InvariantCulture)))
+				.ForMember(x => x.Description,
 					x => x.MapFrom(
 							p => p.Description.Length > ProjectConsants.AllowedCharactersToRender ?
 								string.Concat(
 									p.Description.Substring(0, ProjectConsants.AllowedCharactersToRender),
-									ProjectConsants.DescriptionExtensionStrings) 
+									ProjectConsants.DescriptionExtensionStrings)
 										: p.Description));
 		}
 	}
