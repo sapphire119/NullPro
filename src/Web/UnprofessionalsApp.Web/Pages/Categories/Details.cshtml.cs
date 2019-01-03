b6dd2ee18980f492fd3fe4f0ff4c6a3627f3285f
sapphire119 <sap.phire@abv.bg>
2019-01-03 21:51:58 +0200

    Added Categories/Details functionality need to fix front-end side

---
+++ src/Web/UnprofessionalsApp.Web/Pages/Categories/Details.cshtml.cs
@@ -0,0 +1,46 @@
+namespace UnprofessionalsApp.Web.Pages.Categories
+{
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.AspNetCore.Mvc.RazorPages;
+	using UnprofessionalsApp.DataServices.Contracts;
+	using UnprofessionalsApp.ViewInputModels.ViewModels.Categories;
+	using UnprofessionalsApp.ViewInputModels.ViewModels.Home;
+
+	public class DetailsModel : PageModel
+	{
+		private readonly ICategoriesService categoriesService;
+
+		public DetailsModel(ICategoriesService categoriesService)
+		{
+			this.categoriesService = categoriesService;
+		}
+
+		//Validate Me
+		[BindProperty(SupportsGet = true)]
+		public int CategoryId { get; set; }
+
+		[BindProperty(SupportsGet = true)]
+		public string CategoryName { get; set; }
+
+		public bool AreThereAnyPostsForCategory { get; set; }
+
+		public IEnumerable<PostByCategoryViewModel> RelatedPosts { get; set; }
+
+		public async Task<IActionResult> OnGetAsync()
+		{
+			this.AreThereAnyPostsForCategory =
+				await this.categoriesService.AreThereAnyPostsWithCategory(this.CategoryId);
+
+			if (this.AreThereAnyPostsForCategory)
+			{
+				this.RelatedPosts = await this.categoriesService.GetAllRealtedPosts(this.CategoryId);
+				return Page();
+			}
+			//TODO: Write a Display Error message for lack of posts on details category page.
+			this.ModelState.AddModelError(string.Empty, "Category does not contain any posts.");
+			return this.RedirectToPage("/Categories/Index");
+		}
+	}
+}
