593953c9fbfe9dfc20702e6acc5dba88b2a05ca6
sapphire119 <sap.phire@abv.bg>
2019-01-03 21:51:31 +0200

    Introduced two new view models

---
+++ src/Services/UnprofessionalsApp.ViewInputModels/ViewModels/Categories/PostByCategoryViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using AutoMapper;
+using UnprofessionalsApp.Mapping.Contracts;
+using UnprofessionalsApp.Models;
+using UnprofessionalsApp.ViewInputModels.Extension;
+
+namespace UnprofessionalsApp.ViewInputModels.ViewModels.Categories
+{
+	public class PostByCategoryViewModel : IMapFrom<Post>, IHaveCustomMappings
+	{
+		private string imageUrl;
+		private string description;
+
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public string Description
+		{
+			get
+			{
+				//TODO: Test me
+				var result = this.description.Length > ProjectConsants.AllowedCharactersToRender ?
+								string.Concat(
+									this.description.Substring(0, ProjectConsants.AllowedCharactersToRender),
+									ProjectConsants.DescriptionExtensionStrings)
+										: this.description;
+				return result;
+			}
+			set
+			{
+				this.description = value;
+			}
+		}
+
+		public string ImageUrl
+		{
+			get
+			{
+				//TODO: Test me
+				var result = string.IsNullOrWhiteSpace(this.imageUrl) ?
+					ProjectConsants.DefaultImageUrl : this.imageUrl;
+
+				result = WebUtility.UrlDecode(result);
+
+				return result;
+			}
+			set
+			{
+				this.imageUrl = value;
+			}
+		}
+
+		public int Rating { get; set; }
+
+		public int Popularity { get; set; }
+
+		public DateTime DateOfCreation { get; set; }
+
+		public int UserId { get; set; }
+
+		public string Username { get; set; }
+
+		public void CreateMappings(IMapperConfigurationExpression configuration)
+		{
+			configuration.CreateMap<Post, PostByCategoryViewModel>()
+					.ForMember(x => x.Username, opts => opts.MapFrom(p => p.User.UserName));
+		}
+	}
+}
---
+++ src/Services/UnprofessionalsApp.ViewInputModels/ViewModels/Posts/PostDetailsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using UnprofessionalsApp.Mapping.Contracts;
+using UnprofessionalsApp.Models;
+
+namespace UnprofessionalsApp.ViewInputModels.ViewModels.Posts
+{
+	public class PostDetailsViewModel : IMapFrom<Post>, IHaveCustomMappings
+	{
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public string ImageUrl { get; set; }
+
+		public int Popularity { get; set; }
+
+		public int Rating { get; set; }
+
+		public DateTime DateOfCreation { get; set; }
+
+		public int UserId { get; set; }
+
+		public string Username { get; set; }
+
+		public int CategoryId { get; set; }
+
+		public Guid? FirmId { get; set; }
+
+		public IEnumerable<Tag> Tags { get; set; }
+
+		public void CreateMappings(IMapperConfigurationExpression configuration)
+		{
+			configuration.CreateMap<Post, PostDetailsViewModel>()
+				.ForMember(x => x.Username, opts => opts.MapFrom(p => p.User.UserName))
+				.ForMember(x => x.Tags, opts => opts.MapFrom(p =>p.Tags.Select(t => t.Tag)));
+		}
+	}
+}
