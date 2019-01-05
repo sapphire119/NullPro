1305f8c8adad1ef3f151536c7cd451c65128172c
sapphire119 <sap.phire@abv.bg>
2019-01-05 18:13:40 +0200

    Removed unnecessary files. Implemented PostDetailsViewModel & TagViewModel

--- src/Services/UnprofessionalsApp.ViewInputModels/ViewModels/Posts/PostDetailsViewModel.cs
+++ src/Services/UnprofessionalsApp.ViewInputModels/ViewModels/Posts/PostDetailsViewModel.cs
@@ -5,18 +5,39 @@ using System.Text;
 using AutoMapper;
 using UnprofessionalsApp.Mapping.Contracts;
 using UnprofessionalsApp.Models;
+using UnprofessionalsApp.ViewInputModels.Extension;
+using System.Net;
+using UnprofessionalsApp.ViewInputModels.ViewModels.Tags;
+using UnprofessionalsApp.ViewInputModels.ViewModels.Comments;
 
 namespace UnprofessionalsApp.ViewInputModels.ViewModels.Posts
 {
 	public class PostDetailsViewModel : IMapFrom<Post>, IHaveCustomMappings
 	{
+		private string imageUrl;
+
 		public int Id { get; set; }
 
 		public string Title { get; set; }
 
 		public string Description { get; set; }
 
-		public string ImageUrl { get; set; }
+		public string ImageUrl
+		{
+			get
+			{
+				var result = string.IsNullOrWhiteSpace(this.imageUrl) ?
+					ProjectConsants.DefaultImageUrl : this.imageUrl;
+
+				result = WebUtility.UrlDecode(result);
+
+				return result;
+			}
+			set
+			{
+				this.imageUrl = value;
+			}
+		}
 
 		public int Popularity { get; set; }
 
@@ -32,13 +53,18 @@ namespace UnprofessionalsApp.ViewInputModels.ViewModels.Posts
 
 		public Guid? FirmId { get; set; }
 
-		public IEnumerable<Tag> Tags { get; set; }
+		public string FirmName { get; set; }
+
+		public IEnumerable<CommentViewModel> Comments { get; set; }
+
+		public IEnumerable<TagViewModel> Tags { get; set; }
 
 		public void CreateMappings(IMapperConfigurationExpression configuration)
 		{
 			configuration.CreateMap<Post, PostDetailsViewModel>()
 				.ForMember(x => x.Username, opts => opts.MapFrom(p => p.User.UserName))
-				.ForMember(x => x.Tags, opts => opts.MapFrom(p =>p.Tags.Select(t => t.Tag)));
+				.ForMember(x => x.FirmName, opts => opts.MapFrom(p => p.Firm.Name))
+				.ForMember(x => x.Comments, opts => opts.MapFrom(p => p.Comments));
 		}
 	}
 }
--- src/Services/UnprofessionalsApp.ViewInputModels/ViewModels/Tags/TagViewModel.cs
+++ src/Services/UnprofessionalsApp.ViewInputModels/ViewModels/Tags/TagViewModel.cs
@@ -1,13 +1,20 @@
-﻿using UnprofessionalsApp.Mapping.Contracts;
+﻿using AutoMapper;
+using UnprofessionalsApp.Mapping.Contracts;
 using UnprofessionalsApp.Models;
 
 namespace UnprofessionalsApp.ViewInputModels.ViewModels.Tags
 {
-	public class TagViewModel : IMapFrom<Tag>
+	public class TagViewModel : IMapFrom<TagPost>, IHaveCustomMappings
 	{
 		public int Id { get; set; }
 
 		public string Name { get; set; }
 
+		public void CreateMappings(IMapperConfigurationExpression configuration)
+		{
+			configuration.CreateMap<TagPost, TagViewModel>()
+				.ForMember(t => t.Id, opts => opts.MapFrom(t => t.TagId))
+				.ForMember(t => t.Name, opts => opts.MapFrom(t => t.Tag.Name));
+		}
 	}
 }
