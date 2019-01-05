38f1fdfdd5cfb7d001f8f5818120a590fa72504f
sapphire119 <sap.phire@abv.bg>
2019-01-05 18:15:41 +0200

    Created CommentViewModel & ReplyViewModel

---
+++ src/Services/UnprofessionalsApp.ViewInputModels/ViewModels/Comments/CommentViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using UnprofessionalsApp.Mapping.Contracts;
+using UnprofessionalsApp.Models;
+using UnprofessionalsApp.ViewInputModels.ViewModels.Replies;
+
+namespace UnprofessionalsApp.ViewInputModels.ViewModels.Comments
+{
+	public class CommentViewModel : IMapFrom<Comment>, IHaveCustomMappings
+	{
+		public int Id { get; set; }
+
+		public string Description { get; set; }
+
+		public int Rating { get; set; }
+
+		public DateTime DateOfCreation { get; set; }
+
+		public int UserId { get; set; }
+
+		public string Username { get; set; }
+
+		public int PostId { get; set; }
+
+		public IEnumerable<ReplyViewModel> Replies { get; set; }
+
+		public void CreateMappings(IMapperConfigurationExpression configuration)
+		{
+			configuration.CreateMap<Comment, CommentViewModel>()
+				.ForMember(c => c.Username, opts => opts.MapFrom(c => c.User.UserName));
+		}
+	}
+}
