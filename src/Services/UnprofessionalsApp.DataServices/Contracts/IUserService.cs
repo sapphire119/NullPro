a9ee86d9df990eb8a52ca55624d434bcc2451386
sapphire119 <sap.phire@abv.bg>
2019-01-06 21:16:32 +0200

    Introduced IUserService.cs and implemented into UserService.cs

---
+++ src/Services/UnprofessionalsApp.DataServices/Contracts/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnprofessionalsApp.Models;
+using UnprofessionalsApp.ViewInputModels.ViewModels.Comments;
+using UnprofessionalsApp.ViewInputModels.ViewModels.Posts;
+
+namespace UnprofessionalsApp.DataServices.Contracts
+{
+	public interface IUsersService
+	{
+		Task<TViewModel> GetUserByIdAsync<TViewModel>(int userId);
+	}
+}
---
+++ src/Services/UnprofessionalsApp.DataServices/UserService.cs
@@ -0,0 +1,37 @@
+﻿namespace UnprofessionalsApp.DataServices
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	using UnprofessionalsApp.Common;
+	using UnprofessionalsApp.DataServices.Contracts;
+	using UnprofessionalsApp.Mapping;
+	using UnprofessionalsApp.Models;
+	using UnprofessionalsApp.ViewInputModels.ViewModels.Comments;
+	using UnprofessionalsApp.ViewInputModels.ViewModels.Posts;
+
+	public class UserService : IUsersService
+	{
+		private readonly IRepository<UnprofessionalsAppUser> userRepository;
+
+		public UserService(IRepository<UnprofessionalsAppUser> userRepository)
+		{
+			this.userRepository = userRepository;
+		}
+
+		public Task<TViewModel> GetUserByIdAsync<TViewModel>(int userId)
+		{
+			//TODO: Check if invalid userId
+			var userTask = Task.Run(() => this.userRepository.All()
+						.Where(u => u.Id == userId)
+						.To<TViewModel>()
+						.FirstOrDefault());
+
+			return userTask;
+		}
+	}
+}
