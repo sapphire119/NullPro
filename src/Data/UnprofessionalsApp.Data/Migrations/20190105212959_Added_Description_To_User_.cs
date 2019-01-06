4000643b0898b97aa1d6b0473bc470d91a3622e6
sapphire119 <sap.phire@abv.bg>
2019-01-06 21:15:59 +0200

    Migrations files

---
+++ src/Data/UnprofessionalsApp.Data/Migrations/20190105212959_Added_Description_To_User_.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UnprofessionalsApp.Data.Migrations
+{
+    public partial class Added_Description_To_User_ : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "AspNetUsers");
+        }
+    }
+}
