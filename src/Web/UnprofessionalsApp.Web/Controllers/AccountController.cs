fef70c2350d36e01f7fba7a89e8541ea2dc621ee
sapphire119 <sap.phire@abv.bg>
2019-01-06 21:26:19 +0200

    Removed Razor Page for Account Details and supported logic. Instead created an AccountController class in Controllers/AccountController.cs, and a view in Views/Account/Details.cshtml.

---
+++ src/Web/UnprofessionalsApp.Web/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿namespace UnprofessionalsApp.Web.Controllers
+{
+	using Microsoft.AspNetCore.Mvc;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Threading.Tasks;
+	using UnprofessionalsApp.DataServices.Contracts;
+	using UnprofessionalsApp.ViewInputModels.ViewModels.Users;
+
+	public class AccountController : Controller
+	{
+		private readonly IUsersService usersService;
+
+		public AccountController(IUsersService usersService)
+		{
+			this.usersService = usersService;
+		}
+
+		[Route("Account/Details/{userId?}")]
+		public async Task<IActionResult> Details(int userId)
+		{
+			//TODO: Validate Details Action in AccountController
+			var model = await this.usersService.GetUserByIdAsync<UserDetailsViewModel>(userId);
+			return this.View(model);
+		}
+	}
+}
