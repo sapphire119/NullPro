dbfc38c26f6c7f9112db6f00fcc1b56c248a45a3
sapphire119 <sap.phire@abv.bg>
2019-01-08 01:26:04 +0200

    Started working on Posts/PostCreateProfile.cs

---
+++ src/Services/UnprofessionalsApp.Mapping/Profiles/InputModels/Posts/PostCreateProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnprofessionalsApp.Models;
+using UnprofessionalsApp.ViewInputModels.InputModels.Posts;
+
+namespace UnprofessionalsApp.Mapping.Profiles.InputModels.Posts
+{
+	public class PostCreateProfile : Profile
+	{
+		public PostCreateProfile()
+		{
+			CreateMap<PostCreateInputModel, Post>()
+				.ForMember(x => x.ImageUrl, opts => opts.Ignore())
+				.ForMember(x => x.Title, opts => opts.MapFrom(x => x.Title))
+				.ForMember(x => x.Description, opts => opts.MapFrom(x => x.Description))
+				.ForMember(x => x.CategoryId, opts => opts.MapFrom(x => x.CategoryId));
+		}
+	}
+}
