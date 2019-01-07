c439dfa2666842c9824b1260b0a9021ceadba2ac
sapphire119 <sap.phire@abv.bg>
2019-01-08 01:24:29 +0200

    Created Attribute for file extensions on IFormFile

---
+++ src/Services/UnprofessionalsApp.CustomAttributes/CustomFileExtensionAttribute.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace UnprofessionalsApp.CustomAttributes
+{
+	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+	public class CustomFileExtensionAttribute : ValidationAttribute
+	{
+		private List<string> AllowedExtensions { get; set; }
+
+		public CustomFileExtensionAttribute(string fileExtensions)
+		{
+			this.AllowedExtensions = fileExtensions.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+				.Select(v => v.Trim())
+				.ToList();
+		}
+
+		public override bool IsValid(object value)
+		{
+			IFormFile formFile = value as IFormFile;
+
+			if (formFile != null)
+			{
+				var fileName = formFile.FileName;
+
+				var result = this.AllowedExtensions.Any(e => fileName.EndsWith(e));
+				return result;
+			}
+
+			return base.IsValid(value);
+		}
+	}
+}
