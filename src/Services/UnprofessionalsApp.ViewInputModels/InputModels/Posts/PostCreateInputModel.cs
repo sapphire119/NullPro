bcb73d98646450e2fee5ed1536a71fe7be8bf81f
sapphire119 <sap.phire@abv.bg>
2019-01-08 01:24:59 +0200

    Implemented Input model for Post Creation

---
+++ src/Services/UnprofessionalsApp.ViewInputModels/InputModels/Posts/PostCreateInputModel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using UnprofessionalsApp.CustomAttributes;
+
+namespace UnprofessionalsApp.ViewInputModels.InputModels.Posts
+{
+	public class PostCreateInputModel
+	{
+		[Required]
+		[StringLength(20, MinimumLength = 2, ErrorMessage = "Title must be between {0} and {1} characters")]
+		public string Title { get; set; }
+
+		[Required]
+		[DataType(DataType.MultilineText)]
+		public string Description { get; set; }
+		
+		[CustomFileExtension(".jpg, .png, .jpeg", ErrorMessage = "Accepted file formats are: {0}")]
+		public IFormFile Image { get; set; }
+		
+		public int CategoryId { get; set; }
+
+		[Required]
+		[RegularExpression(@"^\w+[.\-_]*\w*$")]
+		[StringLength(20, MinimumLength = 2)]
+		public string Username { get; set; }
+										   //[Required]
+										   //[StringLength(15, MinimumLength = 3)]
+										   //[RegularExpression(@"^\w+[.\-_]*\w*$")]
+										   //public string Username { get; set; }
+
+		//public int CategoryId { get; set; }
+
+		//[Required]
+		//[RegularExpression(@"^\w+[.\-_]*\w*$")]
+		//public string Username { get; set; }
+
+	}
+}
