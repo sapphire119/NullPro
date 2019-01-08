5c4697d3125c1e5d95a0ba55e0559b7caf27954b
sapphire119 <sap.phire@abv.bg>
2019-01-09 01:53:39 +0200

    User details now correctly redirects to user profile when user Id is the same

--- src/Web/UnprofessionalsApp.Web/Controllers/AccountController.cs
+++ src/Web/UnprofessionalsApp.Web/Controllers/AccountController.cs
@@ -1,25 +1,35 @@
 ﻿namespace UnprofessionalsApp.Web.Controllers
 {
+	using Microsoft.AspNetCore.Identity;
 	using Microsoft.AspNetCore.Mvc;
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
 	using System.Threading.Tasks;
 	using UnprofessionalsApp.DataServices.Contracts;
+	using UnprofessionalsApp.Models;
 	using UnprofessionalsApp.ViewInputModels.ViewModels.Users;
 
 	public class AccountController : Controller
 	{
 		private readonly IUsersService usersService;
+		private readonly UserManager<UnprofessionalsAppUser> userManager;
 
-		public AccountController(IUsersService usersService)
+		public AccountController(IUsersService usersService, UserManager<UnprofessionalsAppUser> userManager)
 		{
 			this.usersService = usersService;
+			this.userManager = userManager;
 		}
 
 		[Route("Account/Details/{userId?}")]
 		public async Task<IActionResult> Details(int userId)
 		{
+			var currentuser = await this.userManager.GetUserAsync(this.User);
+			if (currentuser != null && currentuser.Id == userId)
+			{
+				return this.Redirect("/Identity/Account/Manage");
+			}
+
 			//TODO: Validate Details Action in AccountController
 			var model = await this.usersService.GetUserByIdAsync<UserDetailsViewModel>(userId);
 			return this.View(model);
