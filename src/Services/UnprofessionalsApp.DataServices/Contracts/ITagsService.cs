f01c5f64527d29165991e0f87cb6f89fe92eeb92
sapphire119 <sap.phire@abv.bg>
2019-01-08 09:23:02 +0200

    Created and implemented service to create tags from input data.

---
+++ src/Services/UnprofessionalsApp.DataServices/Contracts/ITagsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UnprofessionalsApp.Models;
+
+namespace UnprofessionalsApp.DataServices.Contracts
+{
+	public interface ITagsService
+	{
+		Task<IEnumerable<Tag>> CreateTags(string tags);
+
+		Task<IEnumerable<Tag>> RemoveDuplicates(IEnumerable<Tag> tags);
+	}
+}
---
+++ src/Services/UnprofessionalsApp.DataServices/TagsService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using UnprofessionalsApp.DataServices.Contracts;
+using UnprofessionalsApp.Models;
+using UnprofessionalsApp.Common;
+
+namespace UnprofessionalsApp.DataServices
+{
+	public class TagsService : ITagsService
+	{
+		private readonly IRepository<Tag> tagsRepository;
+
+		public TagsService(IRepository<Tag> tagsRepository)
+		{
+			this.tagsRepository = tagsRepository;
+		}
+
+		public Task<IEnumerable<Tag>> RemoveDuplicates(IEnumerable<Tag> tags)
+		{
+			var resultingTagsTask = Task.Run(() =>
+			{
+				var result = tags.Where(t => !this.tagsRepository.All().Any(it => it.Name == t.Name));
+
+				return result;
+			}); 
+
+			return resultingTagsTask;
+		}
+
+		public Task<IEnumerable<Tag>> CreateTags(string tags)
+		{
+			var currentTagsTask = Task.Run(() =>
+			{
+				var result = tags.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+							   .Select(tagName => new Tag
+							   {
+								   Name = tagName.Trim()
+							   });
+
+				return result;
+			}); 
+
+			return currentTagsTask;
+		}
+	}
+}
