a366ba30b0e39f1e903ab39e78c0cefb4d6fd44a
sapphire119 <sap.phire@abv.bg>
2019-01-08 09:23:25 +0200

    Created and implemented two dtos for Post and Image

---
+++ src/Services/UnprofessionalsApp.Mapping/Profiles/DataTransferObjects/Images/CreateImageProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnprofessionalsApp.Models;
+
+namespace UnprofessionalsApp.Mapping.Profiles.DataTransferObjects.Images
+{
+	public class CreateImageProfile : Profile
+	{
+		public CreateImageProfile()
+		{
+			CreateMap<string, Image>()
+				.ForMember(x => x.Url, opts => opts.MapFrom(x => x));
+		}
+	}
+}
---
+++ src/Services/UnprofessionalsApp.Mapping/Profiles/DataTransferObjects/Posts/PostCreateProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using UnprofessionalsApp.DataTransferObjects.Posts;
+using UnprofessionalsApp.Models;
+using UnprofessionalsApp.ViewInputModels.InputModels.Posts;
+
+namespace UnprofessionalsApp.Mapping.Profiles.DataTransferObjects.Posts
+{
+	public class PostCreateProfile : Profile
+	{
+		public PostCreateProfile()
+		{
+			CreateMap<PostCreateInputModel, PostCreateDto>()
+				.ForMember(x => x.ImageId, opts => opts.Ignore())
+				//.ForMember(x => x.Tags, opts => opts.Ignore())
+				.ForMember(x => x.Title, opts => opts.MapFrom(x => x.Title))
+				.ForMember(x => x.Description, opts => opts.MapFrom(x => x.Description))
+				.ForMember(x => x.CategoryId, opts => opts.MapFrom(x => x.CategoryId));
+
+			CreateMap<PostCreateDto, Post>()
+				.ForMember(x => x.ImageId, opts => opts.MapFrom(x => x.ImageId))
+				.ForMember(x => x.Title, opts => opts.MapFrom(x => x.Title))
+				.ForMember(x => x.Description, opts => opts.MapFrom(x => x.Description))
+				.ForMember(x => x.CategoryId, opts => opts.MapFrom(x => x.CategoryId))
+				.ForMember(x => x.UserId, opts => opts.MapFrom(x => x.UsernId));
+				//.ForMember(x => x.Tags, opts => opts.MapFrom(x => x.Tags.Select(t => t)));
+		}
+	}
+}
