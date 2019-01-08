090558d490cfb0184439d6506ae99eacb7783f17
sapphire119 <sap.phire@abv.bg>
2019-01-08 09:24:45 +0200

    Fully implemented Post creation logic for application. Data validation for Post creation also successful. PostDto used in Posts/Create Razor Page.

---
+++ src/Services/UnprofessionalsApp.DataTransferObjects/Posts/PostCreateDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnprofessionalsApp.Models;
+
+namespace UnprofessionalsApp.DataTransferObjects.Posts
+{
+	public class PostCreateDto
+	{
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public int ImageId { get; set; }
+
+		public int CategoryId { get; set; }
+
+		public int UsernId { get; set; }
+
+		//public IEnumerable<Tag> Tags { get; set; }
+	}
+}
